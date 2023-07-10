25d412d39120c7a06031410af70d2609b5a436aa
apurbanand <apurb@unitedpro.in>
2023-07-10 22:31:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-// server.js
-const http = require('http')
-const PORT = process.env.PORT || 9000
-http
-  .createServer((req, res) => {
-    console.log('New connection')
-    res.end('Hello Cloud Run - now from continuous build and run')
-  })
-  .listen(PORT, () => console.log('Listening on', PORT))
+const http = require('http');
+const { processDocument } = require('./index');
+
+const PORT = process.env.PORT || 9000;
+
+http.createServer(async (req, res) => {
+  console.log('New connection');
+
+  if (req.method === 'POST') {
+    if (req.url === '/processDocument') {
+      try {
+        const entities = await processDocument(req, res);
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(entities);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+      }
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  } else {
+    res.writeHead(405);
+    res.end();
+  }
+}).listen(PORT, () => console.log('Listening on', PORT));
