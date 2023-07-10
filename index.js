d622e68db957c76e9382ab36abc5dd8b5da2c1e3
apurbanand <apurb@unitedpro.in>
2023-07-10 22:13:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,76 @@
-/**
- * TODO(developer): Uncomment these variables before running the sample.
- */
-const projectId = process.env.PROJECT_ID;
-const location = 'process.env.LOCATION; // Format is 'us' or 'eu'
-const processorId = process.env.PROCESSOR_ID; // Create processor in Cloud Console
-const filePath = process.env.FILE_PATH;
+'use strict';
 
-const {DocumentProcessorServiceClient} =
-  require('@google-cloud/documentai').v1beta3;
+const { DocumentProcessorServiceClient } = require('@google-cloud/documentai').v1beta3;
+const { Storage } = require('@google-cloud/storage');
 
-// Instantiates a client
-const client = new DocumentProcessorServiceClient();
+exports.processDocument = async (req, res) => {
+  const { projectId, location, processorId, fileName } = req.body;
 
-async function processDocument() {
-  // The full resource name of the processor, e.g.:
-  // projects/project-id/locations/location/processor/processor-id
-  // You must create new processors in the Cloud Console first
-  const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
+  // Instantiates a client
+  const client = new DocumentProcessorServiceClient();
+  const storage = new Storage();
 
-  // Read the file into memory.
-  const fs = require('fs').promises;
-  const imageFile = await fs.readFile(filePath);
+  async function processDocument() {
+    // The full resource name of the processor, e.g.:
+    // projects/project-id/locations/location/processor/processor-id
+    // You must create new processors in the Cloud Console first
+    const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
 
-  // Convert the image data to a Buffer and base64 encode it.
-  const encodedImage = Buffer.from(imageFile).toString('base64');
+    // Read the file from Google Cloud Storage
+    const bucketName = 'docai-upload-dropbox';
+    const file = storage.bucket(bucketName).file(fileName);
+    const [fileContent] = await file.download();
 
-  const request = {
-    name,
-    rawDocument: {
-      content: encodedImage,
-      mimeType: 'application/pdf',
-    },
-  };
+    // Convert the file data to a Buffer and base64 encode it.
+    const encodedImage = Buffer.from(fileContent).toString('base64');
 
-  // Recognizes text entities in the PDF document
-  const [result] = await client.processDocument(request);
+    const request = {
+      name,
+      rawDocument: {
+        content: encodedImage,
+        mimeType: 'application/pdf',
+      },
+    };
 
-  console.log('Document processing complete.');
+    // Recognizes text entities in the PDF document
+    const [result] = await client.processDocument(request);
 
-  // Read fields specificly from the specalized US drivers license processor:
-  // https://cloud.google.com/document-ai/docs/processors-list#processor_us-driver-license-parser
-  // retriving data from other specalized processors follow a similar pattern.
-  // For a complete list of processors see:
-  // https://cloud.google.com/document-ai/docs/processors-list
-  //
-  // OCR and other data is also present in the quality processor's response.
-  // Please see the OCR and other samples for how to parse other data in the
-  // response.
-  const {document} = result;
-  for (const entity of document.entities) {
-    // Fields detected. For a full list of fields for each processor see
-    // the processor documentation:
+    console.log('Document processing complete.');
+
+    // Read fields specifically from the specialized US driver's license processor:
+    // https://cloud.google.com/document-ai/docs/processors-list#processor_us-driver-license-parser
+    // Retrieving data from other specialized processors follows a similar pattern.
+    // For a complete list of processors, see:
     // https://cloud.google.com/document-ai/docs/processors-list
-    const key = entity.type;
-    // some other value formats in addition to text are availible
-    // e.g. dates: `entity.normalizedValue.dateValue.year`
-    const textValue =
-      entity.textAnchor !== null ? entity.textAnchor.content : '';
-    const conf = entity.confidence * 100;
-    console.log(
-      `* ${JSON.stringify(key)}: ${JSON.stringify(textValue)}(${conf.toFixed(
-        2
-      )}% confident)`
-    );
+    //
+    // OCR and other data are also present in the quality processor's response.
+    // Please see the OCR and other samples for how to parse other data in the
+    // response.
+    const { document } = result;
+    const entities = [];
+    for (const entity of document.entities) {
+      // Fields detected. For a full list of fields for each processor, see
+      // the processor documentation:
+      // https://cloud.google.com/document-ai/docs/processors-list
+      const key = entity.type;
+      // Some other value formats in addition to text are available
+      // e.g. dates: `entity.normalizedValue.dateValue.year`
+      const textValue =
+        entity.textAnchor !== null ? entity.textAnchor.content : '';
+      const conf = entity.confidence * 100;
+      entities.push({
+        key,
+        textValue,
+        confidence: conf.toFixed(2)
+      });
+    }
+
+    res.status(200).json(entities);
   }
-}
+
+  // [END documentai_process_specialized_document]
+  await processDocument().catch((err) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  });
+};
